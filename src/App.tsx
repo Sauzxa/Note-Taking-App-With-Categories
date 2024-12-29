702bdac90d37ca311bf6bb44741678f5b0947642
Raouf fer <feraouf91@gmail.com>
2024-12-29 20:34:42 +0100

    adding Storage for Notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
 import { Container } from "react-bootstrap"
 import { Routes , Route, Navigate  } from "react-router-dom"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NewNote from './NewNote' 
+// import NoteForm from "./NoteForm";
+import useLocalStorage from "./customeHooks/useLocalStorage";
+
+
+// for each Note has a specific id and NoteData ta3ha
+export type Note = {
+  id : String
+}& NoteData
+
+export  type NoteData ={
+  title : string ,
+  tags : Tag[] ,
+  markdown : string
+
+}
+export type RawNote = {
+  id : string 
+}
+// Notes li 3andhom same tag li rah yatbadal
+export type RawNoteData = {
+  title : string ,
+  markdown : string,
+  tagIds : string[] 
+}
+type Tag = {
+  id : string ,
+  label : string
+}
 const App = () => {
+  const [notes , setNotes] = useLocalStorage<RawNote[]>('Notes' , [])
+  const [tags , setTags] = useLocalStorage<Tag[]>('TAGS' , [])
   return (
    <Container className="my-4">
      <Routes>
       <Route path="/" element={<h1>Home Page</h1>}  />
-     <Route path="new" element={<h1>New</h1>} />
-     <Route path="/:id"  >
+     <Route path="new" element={<NewNote />} />
+     <Route path="/:id" >
      <Route index element={<h1>show</h1>}  />
      <Route path="edit" element={<h1>edit</h1>}  />
      </Route>
---
+++ src/NoteForm.tsx
@@ -0,0 +1,81 @@
+import { FormEvent, useRef, useState } from "react"
+import { Button, Col, Form, Row, Stack } from "react-bootstrap"
+import { Link } from "react-router-dom"
+import CreatableSelect from "react-select/creatable"
+import { NoteData  } from "./App"
+
+type NoteFormProps = {
+    onSubmit: (Data : NoteData) => void
+}
+export const NoteForm = ({onSubmit} : NoteFormProps) => {
+    const titleRef = useRef<HTMLInputElement>(null)
+    const markdownRef = useRef<HTMLTextAreaElement>(null)
+    const [selectedTags , setSelectedTags] = useState<Tag[]>([])
+
+    // function to remove submit event
+    const HandleSubmit =(e : FormEvent)=>{
+        e.preventDefault()
+
+        onSubmit({
+          title : titleRef.current!.value,
+          markdown : markdownRef.current!.value,
+          tags :[]
+        })
+
+        
+    }
+  return (
+ 
+  <Form onSubmit={HandleSubmit} className="mt-4">
+    <Stack gap={4}>
+      <Row>
+        <Col >
+          <Form.Group controlId="title">
+            <Form.Label>Title</Form.Label>
+            <Form.Control ref={titleRef} required /> 
+          </Form.Group>
+        </Col>
+        <Col >
+          <Form.Group controlId="tags">
+            <Form.Label>Tags</Form.Label>
+            <CreatableSelect value={selectedTags.map(tag => {
+     return { label : tag.label , value : tag.id}
+            }
+         
+            )} 
+            
+             onChange={tags => {
+                setSelectedTags(tags.map(tag => {
+                    return {
+                        value : tag.id , label : tag.label
+                    }
+                }))
+
+             }}
+            isMulti/>
+          </Form.Group>
+        </Col>
+      </Row>
+      <Form.Group controlId="markdonw">
+            <Form.Label>Body</Form.Label>
+            <Form.Control ref={markdownRef} required as='textarea' /> 
+          </Form.Group>
+          <Stack gap={2} className="d-flex justify-content-end" direction="horizontal">
+                <Button variant="primary" type="submit">Save</Button>
+                <Link to="..">
+                <Button variant="outline-secondary" type="reset">Cancel</Button>
+                </Link>
+          </Stack>
+    </Stack>
+
+
+
+
+  </Form>
+  
+  
+
+  )
+}
+
+export default NoteForm
