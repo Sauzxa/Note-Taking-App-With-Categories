e500eb9bc5016757dfa6f6f10ce02561b0216c0a
Raouf fer <feraouf91@gmail.com>
2025-07-29 12:26:22 +0100

    synch UI state with localStorage

--- src/customeHooks/useLocalStorage.ts
+++ src/customeHooks/useLocalStorage.ts
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react"
-
+// 
 export default function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
   const [value, setValue] = useState<T>(() => {
+    // nasta3mlo direct setValue , useState to keep UI up to date ki naddiw new tags
+
     const jsonValue = localStorage.getItem(key)
     if (jsonValue == null) {
       if (typeof initialValue === "function") {
-        return (initialValue as () => T)() // assurer bali function type ta3ha generic type T 
+        return (initialValue as () => T)() // ya RawNoteData ya Tag 
       } else {
         return initialValue
       }
@@ -18,5 +20,5 @@ export default function useLocalStorage<T>(key: string, initialValue: T | (() =>
     localStorage.setItem(key, JSON.stringify(value))
   }, [value, key])
 
-  return [value, setValue] as [T, typeof setValue]  // optional
+  return [value, setValue] as [T, typeof setValue]  
 }
